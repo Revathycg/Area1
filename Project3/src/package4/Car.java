7d5a79df37bc63d72476d8072ec97ac10ffd96df
Admin <Admin@DESKTOP-IEPVL3O>
2023-11-07 20:17:21 +0530

    commiting

---
+++ Project3/src/package4/Car.java
@@ -0,0 +1,12 @@
+package package4;
+
+public class Car extends Vehicle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Car obj=new Car();
+System.out.println(obj.id);
+System.out.println(obj.color);
+	}
+
+}
